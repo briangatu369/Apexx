35a8139e7998104163001f65b1c5cfd46babccf5
brian <brianagatundu@gmail.com>
2024-12-17 21:44:50 +0300

    add setCookies functionality

---
+++ backend/cookies/createCookies.ts
@@ -0,0 +1,26 @@
+import { Response } from "express";
+
+const maxAgeAccess = 60 * 1000 * 60 * 24;
+//  maxAgeAccess,constant = 1000
+const maxAgeRefresh = 60 * 1000 * 60 * 24 * 30;
+//  maxAgeRefresh,constant = 1000
+
+export const createAuthenticationCookies = (
+  res: Response,
+  accessToken: string,
+  refreshToken: string
+) => {
+  res.cookie("access-token", accessToken, {
+    httpOnly: true,
+    maxAge: maxAgeAccess,
+    sameSite: "strict",
+    secure: false,
+  });
+
+  res.cookie("refresh-token", refreshToken, {
+    httpOnly: true,
+    maxAge: maxAgeRefresh,
+    sameSite: "strict",
+    secure: false,
+  });
+};
