c5ccdf2629eb4dd30f39c5602d38ad8ff785cc18
brian <brianagatundu@gmail.com>
2024-12-17 21:50:21 +0300

    start server

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import connectDb from "./db/mongoDbConnect";
+import userRouter from "./router/user";
 
 const app = express();
 
+app.use(express.json());
+
+app.use("/api/user-service", userRouter);
+
 const startServer = async () => {
   try {
-    await connectDb("mongodb://localhost:2707/apexx");
+    await connectDb("mongodb://localhost:27017/apexx");
 
     app.listen(4000, () => {
       console.log("the server is running");
--- backend/models/user.ts
+++ backend/models/user.ts
@@ -1,6 +1,8 @@
-import mongoose, { Document } from "mongoose";
+import mongoose, { Document, Model, model } from "mongoose";
+import jwt from "jsonwebtoken";
+import bycrpt from "bcryptjs";
 
-const kenyaPhoneNumberRegex = /^(07|01)\d{8}$/;
+export const kenyaPhoneNumberRegex = /^(07|01)\d{8}$/;
 
 interface UserInterface extends Document {
   username: string;
@@ -14,12 +16,18 @@ interface UserInterface extends Document {
   resetPasswordExpires?: Date;
   createdAt?: Date;
   updatedAt?: Date;
+
+  generateAuthToken: () => any;
 }
 
-const userSchema = new mongoose.Schema<UserInterface>(
+interface UserStaticMethods extends Model<UserInterface> {
+  findByCred: (phoneNumber: string, password: string) => Promise<UserInterface>;
+}
+
+const userSchema = new mongoose.Schema(
   {
     username: { type: String, unique: true, required: true },
-    profileImage: { type: String },
+    profileImage: { type: String, default: "" },
     role: {
       type: String,
       enum: ["user", "admin"],
@@ -38,12 +46,40 @@ const userSchema = new mongoose.Schema<UserInterface>(
       default: "active",
     },
     password: { type: String, required: true },
-    resetPasswordToken: { type: String },
-    resetPasswordExpires: { type: Date },
+    resetPasswordToken: { type: String, default: "" },
+    resetPasswordExpires: { type: Date, default: null },
   },
   { timestamps: true }
 );
 
-const User = mongoose.model<UserInterface>("User", userSchema);
+userSchema.statics.findByCred = async function (
+  phoneNumber: string,
+  password: string
+) {
+  const user = await User.findOne({ phoneNumber });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const isMatch = await bycrpt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("Incorrect Password");
+  }
+
+  return user;
+};
+
+userSchema.methods.generateAuthToken = function () {
+  const { password, ...rest } = this;
+
+  const accessToken = jwt.sign(rest, "12345");
+  const refreshToken = jwt.sign(rest, "aljsdflajfl;sdj");
+
+  return { accessToken, refreshToken };
+};
+
+const User = model<UserInterface, UserStaticMethods>("User", userSchema);
 
 export default User;
