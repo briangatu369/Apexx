9d40b9d226e8e2b0add11cb96f601c30150b2c2c
brian <brianagatundu@gmail.com>
2025-01-08 15:58:47 +0300

    update betHistory model

--- backend/models/game/betHistory.ts
+++ backend/models/game/betHistory.ts
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 import { Document } from "mongoose";
+import { GameState } from "./sessionAnalytics";
 
 interface BetHistoryI extends Document {
   userId: mongoose.Schema.Types.ObjectId;
   sessionId: string;
   betAmount: number;
+  status: GameState;
   cashoutMultiplier: number | null;
   payout: number | null;
 }
@@ -17,6 +19,7 @@ const betHistorySchema = new mongoose.Schema<BetHistoryI>(
       required: true,
     },
     sessionId: { type: String, required: true },
+    status: { enum: Object.values(GameState), default: GameState.RUNNING },
     betAmount: { type: Number, required: true },
     cashoutMultiplier: { type: Number, default: null },
     payout: { type: Number, default: null },
