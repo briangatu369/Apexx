cb16be04d29bb70396ec91ed619df735d3069466
brian <brianagatundu@gmail.com>
2025-02-07 14:59:15 +0300

    feat: add user routes

---
+++ backend/src/routes/user.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import UserController from "../controllers/user";
+import validateJwt from "../middlewares/validateJwt";
+
+const userController = new UserController();
+
+const router = Router();
+
+router.post("/register", userController.createUser);
+router.post("/login", userController.login);
+router.post("/verifyjwt", validateJwt, userController.verifyUserAuthentication);
+router.get("/logout", userController.logout);
+
+export default router;
